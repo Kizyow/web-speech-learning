d90824c7c9e06ae6603990a934015814d1c3082b
Delioos <htz_j@protonmail.com>
2023-12-05 16:11:42 +0100

    feat(play): starter (buittons and modified events) for 2nd game

--- src/js/app.js
+++ src/js/app.js
@@ -10,15 +10,59 @@ if ("speechSynthesis" in window) {
     "speechSynthesis supported";
   
   main();
+
+ //  A REFACTO POUR FAIRE PROPRE
+  let result = document.getElementById("result");
+
+  let modeTitle = document.getElementById("currentMode");
+  let mode = "learn"; // default setting
   
+  let playButton = document.getElementById("play")
+  let modeButton = document.getElementById("modeButton");
+  modeButton.addEventListener("click", function (e) {
+    if (mode == "learn") {
+      mode = "play";
+      modeTitle.innerHTML = "Play";
+      modeButton.innerHTML = "click to Learn";
+      // activate play button 
+      playButton.removeAttribute("disabled")
+    } else {
+      mode = "learn";
+      modeTitle.innerHTML = "Learn";
+      modeButton.innerHTML = "click to Play";
+      playButton.setAttribute("disabled", "");
+      cleanResult()
+    }
+  });
+
+
+  playButton.addEventListner("click", function(e) {
+       
+  })
+
+  // introduire une streak une fois que ca fonctionnera 
   let divImages =  document.getElementById('reco');
   let images = getImagesVoitures();
+  let score = document.getElementById("score")
   images.forEach(image => {
     divImages.appendChild(image);
 
     image.addEventListener("click", function (e) {
       console.log("img clicked")
-      speak(image.alt);
+      if (mode == "learn") {
+        speak(image.alt);
+      } else {
+        // handle game here 
+        // choisir un nombre en fonction de la taile du nombre de fils 
+         
+        // dire le nom de l alt par la voix
+        speak(image.alt)
+
+        // attendre la reponse de l utilisateur
+        // si c est bon dire bravo et ajouter un point
+
+      }
+
     });
   });
 
